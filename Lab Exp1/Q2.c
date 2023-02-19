2da2287164b3e7b8774c9ba3b38cbfe8491e30ee
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-19 14:53:21 +0530

    inserting an element in an array

--- Lab Exp1/Q2.c
+++ Lab Exp1/Q2.c
@@ -3,7 +3,12 @@
 int main(){
     int arr[]={1,2,5,7,8};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int j=n;
+    int i,j;
+    printf("The original elements are :\n");
+    for(i=0;i<n;i++) {
+        printf("arr[%d] = %d \n",i,arr[i]);
+    }
+    j=n;
     n=n+1; //for new elemnt size will increase by 1
     int k=3;   //new element at 3rd position(2nd index)
     while(j>=k){
@@ -11,7 +16,7 @@ int main(){
         j--;
     }
     arr[k]=10;
-    int i;
+    printf("After inserting an element:\n");
     for(i=0;i<n;i++){
         printf("arr[%d]=%d",i,arr[i]);
     }
