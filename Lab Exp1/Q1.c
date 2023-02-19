412f6cc129881b44a8e21d8af704d2f2db3c0574
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-19 14:36:57 +0530

    finding the length of array

--- Lab Exp1/Q1.c
+++ Lab Exp1/Q1.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main(){
-    int arr[]={4,3,2,4,3,6,3,6,4,6,3,6,4,6,4,33,5,7,5,34,6,7,46,4,6,5,4,5,5,6,4,8};
+    int arr[]={4,3,2,4,3,6,3,6,4};
     int arr_size=sizeof(arr)/sizeof(arr[0]); //getting length of the array
     printf("The size of array: %d\n",arr_size);
     int i;
