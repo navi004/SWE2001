1822e980a1f1636ae3a7c7cebd9c1e6b2954901a
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-23 07:49:54 +0530

    to search an element in an array

--- Lab Exp1/Q4.c
+++ Lab Exp1/Q4.c
@@ -1,18 +1,23 @@
+// Create a program to find the position of an element in an array
 #include <stdio.h>
 int main() {
   int arr[] = {10,20,30,40,50};
   int n = sizeof(arr)/sizeof(arr[0]);
-  int i=0,j=0;
+  int i=0,j=0,l;
   printf("The original elements are: \n");
   for(i=0;i<n;i++) {
     printf("arr[%d] = %d \n",i,arr[i]);
   }
-  int k =2;
+  int k =10;
   while(j<n) {
     if(arr[j] == k) {
+        l=1;
+ printf("Found the element %d at %dth position",k,j+1);
       break;
     }
     j++;
   }
-  printf("Found the element %d at %d position",k,j+1);
+ 
+  if(l==0)
+  printf("Element not found in the array");
    }
