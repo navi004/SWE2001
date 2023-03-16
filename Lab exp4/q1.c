21019d63614d00c3fddab8d54e3ab6ea781984a7
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-16 23:57:09 +0530

    Update q1.c

--- Lab exp4/q1.c
+++ Lab exp4/q1.c
@@ -1,36 +1,89 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#define SIZE 10;
-
-int queue[SIZE];
+#include <stdlib.h>
+#define SIZE 10
 int front = -1,rear =-1;
+int queue[SIZE];
 
-void Enqueue(int value) {
+void enqueue() {
+    int value;
     if(rear == SIZE-1) {
-        printf("Over flow\n");
+        printf("Overflow\n");
+        return ;
     }
     else {
+        if(front == -1) {
+            front = 0;
+        }
+        printf("Enter the value: ");
+        scanf("%d",&value);  
         rear++;
         queue[rear] = value;
-        printf("INSERTION SUCCESSFUL\n");
+        printf("The enqueued value is %d\n",queue[rear]);
     }
 }
 
-void Dequeue() {
-    if(front == -1) {
+void dequeue() {
+    if(front == -1 || front > rear) {
         printf("Under flow\n");
+        return ;
     }
-    else{
+    else {
+        printf("The dequeued element is %d\n",queue[front]);
         front++;
-        if(front == rear) {
-            front = -1;
-            rear = -1;
-            printf("DELETION SUCCESSFUL\n");
-        }
     }
+}
+
+void display() {
+    if(front == -1 || front > rear) {
+        printf("Queue is empty\n");
+        return;
+    }
+    else {
+        printf("The queue is: ");
+        for(int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+
+void peek() {
+    if(front == -1 || front > rear) {
+        printf("Underflow\n");
+        return ;
+    }
+    printf("the first value in the queue is %d",queue[front]);
 }
 int main() {
     int choice;
-    printf("Enter the ")
+
+    while(1) {
+        printf("\nEnter the choice: \n");
+        printf("1.Enqueue\n");
+        printf("2.Dequeue\n");
+        printf("3.Peek\n");
+        printf("4.Display\n");
+        printf("5.Exit\n");
+        scanf("%d",&choice);
+
+        switch(choice) {
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(0);
+            default :
+                printf("INVALID OPTION");
+        }
+    }
     return 0;
 }
