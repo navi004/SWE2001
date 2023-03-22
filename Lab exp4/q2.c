a26b0951baa7a6481eed4389f1e97296934fae40
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-23 01:05:46 +0530

    circular queue

--- Lab exp4/q2.c
+++ Lab exp4/q2.c
@@ -1,42 +1,92 @@
-// Online C compiler to run C program online
+//write a c program for circular queue
 #include <stdio.h>
-#define SIZE 10
-int circularQueue[SIZE];
-int front =-1,rear =-1;
+#include <stdlib.h>
+# define N 5
 
-void enqueue(int value) {
-    if(front == -1 && rear == -1) {
-        front = 0;
-        rear = 0;
-        circularQueue[rear] = value;
-    }
-    else if((rear+1)%SIZE == front) {
-        printf("Over flow\n");
+int cqueue[N];
+int front = -1,rear = -1;
+
+void enQueue() {
+    int value;
+    if(front == (rear+1)%N) {
+        printf("The Queue is full\n");
+        return;
     }
     else {
-        rear = (rear+1)%SIZE;
-        circularQueue[rear] = value;
+        if(front == -1 && rear == -1) {
+            front = 0;
+        }
+        printf("Enter the value: ");
+        scanf("%d",&value);
+        rear = (rear+1)%N;
+        cqueue[rear] = value;
+        printf("The enqueued element is %d\n",cqueue[rear]);
     }
 }
-
-void dequeue() {
-    if(front == -1) {
-        printf("Under flow\n");
+void deQueue() {
+    if(front == -1 && rear == -1) {
+        printf("The Queue is empty\n");
     }
     else if(front == rear) {
-        printf("The dequeue element is %d",circularQueue[front]);
         front = -1;
         rear = -1;
     }
-    else{
-        printf("The dequeue element is %d",circularQueue[front]);
-        front = (front+1)%SIZE;
+    else {
+        printf("The DeQueued element is %d\n",cqueue[front]);
+        front = (front+1)%N;
+    }
+}
+void peek() {
+    if(front == -1 && rear == -1) {
+        printf("The Queue is empty\n");
+        return;
+    }
+    else {
+        printf("The first element of the queue is %d \n",cqueue[front]);
+    }
+}
+void display() {
+    if(front == -1 && rear == -1) {
+        printf("The Queue is empty \n");
+        return;
+    }
+    else {
+        int i = front;
+        printf("The Queue is : ");
+        while(i != rear) {
+            printf("%d ",cqueue[i]);
+            i = (i+1)%N;
+        }
+        printf("%d\n",cqueue[rear]);
     }
 }
 
-
 int main() {
-    printf("Enter your ch")
+    int choice;
+    while(1) {
+        printf("Enter the choice :\n");
+        printf("1.Enqueue \n2.Dequeue \n3.Peek \n4.Display \n5.Exit \n");
+        scanf("%d",&choice);
 
+        switch(choice) {
+            case 1:
+            enQueue();
+            break;
+            case 2:
+            deQueue();
+            break;
+            case 3:
+            peek();
+            break;
+            case 4:
+            display();
+            break;
+            case 5:
+            exit(0);
+            break;
+            default :
+            printf("INVALID OPTION");
+        }
+    }
     return 0;
 }
