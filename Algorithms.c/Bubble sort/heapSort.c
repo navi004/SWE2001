dfc99376c002fba2c5acc15693a57078a7edc9a8
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-06-01 13:52:10 +0530

    Create heapSort.c

---
+++ Algorithms.c/Bubble sort/heapSort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize the largest as the root
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2; // Right child
+
+    // If the left child is larger than the root
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // If the right child is larger than the largest so far
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // If the largest is not the root, swap and heapify the affected subtree
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    // Build a max heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // Extract elements from the heap one by one
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]); // Move current root to the end
+        heapify(arr, i, 0); // Heapify the reduced heap
+    }
+}
+
+int main() {
+    int arr[] = {4, 8, 1, 9, 3, 2, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
